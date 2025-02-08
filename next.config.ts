d54ce4aaf8085246a68dacaa327f401bd9d75b7a
WiesHerd <wherdzik@gmail.com>
2025-02-08 15:51:43 -0700

    Disable ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  eslint: {
+    // Disable ESLint during build
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig;
